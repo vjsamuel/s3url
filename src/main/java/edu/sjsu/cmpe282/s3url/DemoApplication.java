132a0c8057df0b1823da39e44ba0ad4637d8f50b
Vijay Samuel <vija@ebay.com>
2018-05-06 20:46:24 -0700

    Initial import

---
+++ src/main/java/edu/sjsu/cmpe282/s3url/DemoApplication.java
@@ -0,0 +1,12 @@
+package edu.sjsu.cmpe282.s3url;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
